171bb9cbc782a994ea7fc2e7d6328a7c9bd7b72e
Dan Näsman <dan.nasman@gmail.com>
2019-05-03 17:35:57 +0300

    made a function for the calculations

--- main.py
+++ main.py
@@ -8,59 +8,38 @@ import random;
 import matplotlib.mlab as mlab;
 import math
 
+def getGoals(team):
+    matches = sum(team["xGoalGames"])
+    px = [];
+    pxMu = [];
+    c = 0.0;
+    for x in team["xGoalGames"]:
+        p = float(x / matches);
+        px.append(float(p * c));
+        c += 1;
+    mu = float(sum(px));
+    c = 0.0;
+    for x in team["xGoalGames"]:
+        p = float(x / matches);
+        pxMu.append(float(p * pow((c - mu), 2)))
+        c += 1;
+    sig = float(math.sqrt(float(sum(pxMu))));
+    
+    randomFloat = float(random.uniform(0, 1));
+    goals = 0;
+    psum = 0.0;
+    j = 0.0;
+    for i in range(0, len(team["xGoalGames"])):
+        psum = psum + mlab.normpdf(i, mu, sig);
+        if(randomFloat <= psum and randomFloat > j):
+            goals = i;
+            break;
+    return goals;
+    
 
 def play_match(team1, team2):
-    matches1 = sum(team1[1]["xGoalGames"])
-    px1 = [];
-    pxMu1 = [];
-    c1 = 0.0;
-    for x in team1[1]["xGoalGames"]:
-        p = float(x / matches1);
-        px1.append(float(p * c1));
-        c1 += 1;
-    mu1 = float(sum(px1));
-    c1 = 0.0;
-    for x in team1[1]["xGoalGames"]:
-        p = float(x / matches1);
-        pxMu1.append(float(p * pow((c1 - mu1), 2)))
-        c1 += 1;
-    sig1 = float(math.sqrt(float(sum(pxMu1))));
-    
-    random1 = float(random.uniform(0, 1));
-    goals1 = 0;
-    psum1 = 0.0;
-    j1 = 0.0;
-    for i in range(0, len(team1[1]["xGoalGames"])):
-        psum1 = psum1 + mlab.normpdf(i, mu1, sig1);
-        if(random1 <= psum1 and random1 > j1):
-            goals1 = i;
-            break;
-    
-    matches2 = sum(team2[1]["xGoalGames"])
-    px2 = [];
-    pxMu2 = [];
-    c2 = 0.0;
-    for x in team2[1]["xGoalGames"]:
-        p = float(x / matches2);
-        px2.append(float(p * c2));
-        c2 += 1;
-    mu2 = float(sum(px2));
-    c2 = 0.0;
-    for x in team2[1]["xGoalGames"]:
-        p = float(x / matches2);
-        pxMu2.append(float(p * pow((c2 - mu2), 2)))
-        c2 += 1;
-    sig2 = float(math.sqrt(float(sum(pxMu2))));
-    
-    random2 = float(random.uniform(0, 1));
-    goals2 = 0;
-    psum2 = 0.0;
-    j2 = 0.0;
-    for i in range(0, len(team2[1]["xGoalGames"])):
-        psum2 = psum2 + mlab.normpdf(i, mu2, sig2);
-        if(random2 <= psum2 and random2 > j2):
-            goals2 = i;
-            break;
+    goals1 = getGoals(team1[1]);
+    goals2 = getGoals(team2[1]);
         
     team1[1]["goalsFor"] += goals1;
     team1[1]["goalsAgainst"] += goals2;
